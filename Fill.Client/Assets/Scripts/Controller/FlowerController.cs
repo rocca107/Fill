00ab4858f3203c2d44ca148388f88c9087a7c8d2
rocca107 <heretic.virtue23@gmail.com>
2018-09-03 18:02:42 +0900

    [add] dotweeeen

---
+++ Fill.Client/Assets/Scripts/Controller/FlowerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class FlowerController : MonoBehaviour 
+{
+
+    private bool isAnimation = false;
+    void Start()
+    {
+        isAnimation = false;
+    }
+
+    void Update()
+    {
+        if (!isAnimation)
+            SetAnimation();
+    }
+
+    void SetAnimation()
+    {
+        var rect = gameObject.GetComponent<RectTransform>();
+        rect.DORotate(new Vector3(0f, 0f, 180f), 0.5f).OnComplete(
+            () => {
+                rect.DORotate(new Vector3(0f, 0f, 0), 0.7f);
+            }).SetLoops(-1);
+
+        isAnimation = true;
+    }
+}
